b666eceb04797694ec42fda0a7f5fdd66826871c
daehan <dh16931@gmail.com>
2021-02-19 22:46:34 +0900

    feat: add tables and change db params to kwargs

--- core/db.py
+++ core/db.py
@@ -5,12 +5,15 @@ from contextlib import contextmanager
 import pymysql
 from dotenv import load_dotenv
 
+from core import errors
+
 
 # load dotenv in the base root
 APP_ROOT = os.path.join(os.path.dirname(__file__), '..')   # refers to application_top
 dotenv_path = os.path.join(APP_ROOT, '.env')
 load_dotenv(dotenv_path)
 
+db_host_dev = os.getenv('DB_HOST_DEV')
 db_host = os.getenv('DB_HOST')
 db_port = int(os.getenv('DB_PORT'))
 db_user = os.getenv('DB_USERNAME')
@@ -18,14 +21,26 @@ db_pw = os.getenv('DB_PASSWORD')
 db_dataset = os.getenv('DB_DATABASE')
 db_charset = os.getenv('DB_CHARSET')
 
+db_info_kwargs = {
+    "host": db_host_dev,
+    "port": db_port,
+    "user": db_user,
+    "password": db_pw,
+    "db": db_dataset,
+    "charset": db_charset,
+    "cursorclass": pymysql.cursors.DictCursor
+}
 
 @contextmanager
 def get_db():
     try:
         conn = None
-        conn = pymysql.connect(
-            host=db_host, port=db_port, user=db_user, password=db_pw, db=db_dataset, 
-            charset=db_charset, cursorclass=pymysql.cursors.DictCursor)
+        try:
+            conn = pymysql.connect(**db_info_kwargs)
+        except:
+            print("db host dev connect exception")
+            db_info_kwargs["host"] = db_host
+            conn = pymysql.connect(**db_info_kwargs)
         yield conn
     except:
         traceback.print_exc()
--- core/schema.py
+++ core/schema.py
@@ -2,12 +2,51 @@ schema="""
 CREATE TABLE IF NOT EXISTS `user` (
     `id`                    INT(11) NOT NULL AUTO_INCREMENT,
     `name`                  VARCHAR(50) UNIQUE,
-    `email`                 VARCHAR(50) UNIQUE,
+    `email`                 VARCHAR(50),
     `user_type`             INT(3) DEFAULT 2,
     `login_counting`        INT(5) DEFAULT 0,
     `create_datetime`       TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     `update_datetime`       TIMESTAMP,
     PRIMARY KEY(`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `recipe` (
+    `id`                    INT(11) NOT NULL AUTO_INCREMENT,
+    `name_kr`               VARCHAR(200),
+    `name_en`               VARCHAR(200),
+    `desc_kr`               VARCHAR(1000),
+    `desc_en`               VARCHAR(1000),
+    `image_urls`            VARCHAR(1000),
+    `products_urls`         VARCHAR(1000),
+    `taken_time`            INT(3),
+    `food_type`             INT(3),
+    `create_datetime`       TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    `update_datetime`       TIMESTAMP,
+    PRIMARY KEY(`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `comment` (
+    `id`                    INT(11) NOT NULL AUTO_INCREMENT,
+    `user_id`               INT(11),
+    `text`                  VARCHAR(1000),
+    `create_datetime`       TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    `update_datetime`       TIMESTAMP,
+    PRIMARY KEY(`id`),
+    CONSTRAINT FOREIGN KEY (`user_id`) REFERENCES `cpg`.`user` (`id`) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS `user_recipe` (
+    `user_id`               INT(11) NOT NULL,
+    `recipe_id`             INT(11) NOT NULL,
+    CONSTRAINT FOREIGN KEY (`user_id`) REFERENCES `cpg`.`user` (`id`) ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT FOREIGN KEY (`recipe_id`) REFERENCES `cpg`.`recipe` (`id`) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS `user_follower` (
+    `user_id`               INT(11) NOT NULL,
+    `follower_id`           INT(11) NOT NULL,
+    CONSTRAINT FOREIGN KEY (`user_id`) REFERENCES `cpg`.`user` (`id`) ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT FOREIGN KEY (`follower_id`) REFERENCES `cpg`.`user` (`id`) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS `session` (
